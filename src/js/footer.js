a657223295affe73ee3675f04b9eb4a3c0e4442e
grzeli <grzelak.szymon@gmail.com>
2018-08-17 19:57:47 +0200

    comit pierdoly

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,14 +1,14 @@
 (function($) {
     $(function() {
         $('.footer ul li  > h5').click(function(e) {
-            if($(window) .width() < 576){
+            if($( window ).width() < 576){
             $(this).siblings('#services-list').toggle();
             $('#services-list').not($(this).siblings()).hide();
             e.stopPropagation();
             }
         });
         $('html').click(function() {
-            if($(window) .width() < 576){
+            if($(window).width() < 576){
             $('#services-list').hide();
             }
         });
