ccab98592030881a5ea78827eecedfa3dfd3c9c9
grzeli <grzelak.szymon@gmail.com>
2018-08-17 16:53:53 +0200

    wywalenie clasy z navigacji i dodanie id

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,12 +1,12 @@
 (function($) {
     $(function() {
         $('nav ul li > a:not(:only-child)').click(function(e) {
-            $(this).siblings('.nav-dropdown').toggle();
-            $('.nav-dropdown').not($(this).siblings()).hide();
+            $(this).siblings('#nav-dropdown').toggle();
+            $('#nav-dropdown').not($(this).siblings()).hide();
             e.stopPropagation();
         });
         $('html').click(function() {
-            $('.nav-dropdown').hide();
+            $('#nav-dropdown').hide();
         });
     });
     document.querySelector('#nav-toggle').addEventListener('click', function() {
