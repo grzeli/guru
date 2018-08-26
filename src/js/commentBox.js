3070095a5f5982aed5cec68e1ece72db8a0c20c2
grzeli <grzelak.szymon@gmail.com>
2018-08-26 19:26:53 +0200

    comment box

---
+++ src/js/commentBox.js
@@ -0,0 +1,36 @@
+function render(data){
+    var html =   "<div class='comment'><div class='leftPanelImg'><img src='img/person.jpg' alt='avatar'/></div><div class='rightPanel'><span>"+data.name+"</span><div class='email'>"+data.email+"</div><p>"+data.body+"</p></div></div></div>";
+    $('#commentBox').append(html);
+}
+
+$(document).ready(function(){
+
+    var coment = [];
+
+    if(!localStorage.comentData){
+        localStorage.comentData = [];        
+    }else{
+        coment = JSON.parse(localStorage.comentData);
+    }
+
+
+    for ( var i=0;i<coment.length;i++){
+        render(coment[i]);
+    }
+
+    $('#addComent').click(function(){
+        var addObj = {
+            "name": $('#name').val(),
+            "email": $('#email').val(),
+            "body": $('#bodyText').val()
+        };
+        coment.push(addObj);
+        localStorage.comentData = JSON.stringify(coment);
+        render(addObj);
+        $('#name').val('');
+        $('#email').val('');
+        $('#bodyText').val('');
+
+    });
+
+});
